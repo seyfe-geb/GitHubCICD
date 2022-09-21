c85aea5b5f7d92667ae0d27ef563f38f4d66c757
Seyfegebriel <syberglife@gmail.com>
2022-09-21 12:04:28 -0700

    Initial commit

---
+++ src/main/java/net/seyfe/GitHubCICD/GitHubCicdApplication.java
@@ -0,0 +1,21 @@
+package net.seyfe.GitHubCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitHubCicdApplication {
+
+	@GetMapping("/greeting")
+	public String welcome(){
+		return "Welcome to Seyfe's";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/seyfe/GitHubCICD/GitHubCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package net.seyfe.GitHubCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
